57a71ebf67864fab45fc21e0ecd90aa55bb2d4c3
Connor Bullock <cbullock1@ufl.edu>
2022-06-28 15:09:20 -0400

    Change urls to use enviroment variable

--- src/app/Service/flash-card.service.ts
+++ src/app/Service/flash-card.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlashCardService {
   subjectNotifer: Subject<void> = new Subject<void>();
-  portNumber:number = 8080;
-
+  //portNumber:number = 8080;
+  private apiServerUrl = environment.apiBaseUrl 
   constructor(private http: HttpClient) { }
 
   notifyAboutChange(){
@@ -17,36 +18,36 @@ export class FlashCardService {
 
   // all flashcards
   flashCardList(): Observable<any>{
-    return this.http.get(`http://localhost:${this.portNumber}/api/flashcards`);
+    return this.http.get(`${this.apiServerUrl}/api/flashcards`);
   }
 
   // flash cards by setId
   flashCardBySetId(setId: number):Observable<any>{
-    return this.http.get(`http://localhost:${this.portNumber}/api/flashcards/getSet/${setId}`);
+    return this.http.get(`${this.apiServerUrl}/api/flashcards/getSet/${setId}`);
   }
 
   // flash card by fCardId
   flashCardByCardId(fCardId: number): Observable<any>{
-    return this.http.get(`http://localhost:${this.portNumber}/api/flashcards/${fCardId}`);
+    return this.http.get(`${this.apiServerUrl}/api/flashcards/${fCardId}`);
   }
 
   // add flash card by Flashcard object
   addFlashCard(flashcard: any): Observable<any>{
-    return this.http.post(`http://localhost:${this.portNumber}/api/flashcards`, flashcard);
+    return this.http.post(`${this.apiServerUrl}/api/flashcards`, flashcard);
   }
 
   // update flash card by Flashcard object
   updateFlashCard(flashcard: any): Observable<any>{
-    return this.http.put(`http://localhost:${this.portNumber}/api/flashcards`, flashcard);
+    return this.http.put(`${this.apiServerUrl}/api/flashcards`, flashcard);
   }
 
   // delete flash card by fCardId
   deleteFlashCardById(fCardId: number): Observable<any>{
-    return this.http.delete(`http://localhost:${this.portNumber}/api/flashcards/${fCardId}`);
+    return this.http.delete(`${this.apiServerUrl}/api/flashcards/${fCardId}`);
   }
 
   // userId by flashcard setId
   getUserIdByfCardSetId(setId: number): Observable<any>{
-    return this.http.get(`http://localhost:${this.portNumber}/api/flashcards/getOwner/${setId}`)
+    return this.http.get(`${this.apiServerUrl}/api/flashcards/getOwner/${setId}`)
   }
 }
