10910f717d5d9eac3b3a281b0552657c4f6ef9c9
Jeffrey Saelee <gwfds123@gmail.com>
2022-06-28 10:31:56 -0700

    keep sidenav open at start

--- src/app/Service/drawer.service.ts
+++ src/app/Service/drawer.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DrawerService {
-  isExpanded: boolean = false;
+  isExpanded: boolean = true;
   constructor() { }
 }
